314a111f55428a217a4c47442cdac2b7d8194e74
James Kuehl <james.kuehl@email.com>
2026-05-15 19:40:19 -0300

    Teste de envio de arquivos

---
+++ EstruturaWhile.java
@@ -0,0 +1,10 @@
+public class EstruturaWhile {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 6) {
+            System.out.println("Número: " + i);
+            i++;
+        }
+    }
+}
